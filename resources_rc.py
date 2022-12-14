fd0db0837675c128e08f1ad35189459d443a0376
jean <jean@nc-ass-vip.sdv.fr>
2022-12-14 18:12:14 +0100

    scroll

--- resources_rc.py
+++ resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt5 (Qt v5.9.5)
+# Created by: The Resource Compiler for PyQt5 (Qt v5.15.3)
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -72,8 +72,8 @@ qt_resource_struct_v2 = b"\
 \x00\x00\x01\x61\x51\x0b\x33\x40\
 "
 
-qt_version = QtCore.qVersion().split('.')
-if qt_version < ['5', '8', '0']:
+qt_version = [int(v) for v in QtCore.qVersion().split('.')]
+if qt_version < [5, 8, 0]:
     rcc_version = 1
     qt_resource_struct = qt_resource_struct_v1
 else:
